0b08ab9217ecdf034c4dc0c5598767722aa7f3d1
Your Name <godfreymakara2017@yahoo.com>
2019-07-30 17:52:59 +0300

    pushing files for remote deployment

--- makarablog/settings.py
+++ makarablog/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'makarablog.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'makarablog',
-        'USER': 'godfrey',
-        'PASSWORD': 'kiugokiega',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
